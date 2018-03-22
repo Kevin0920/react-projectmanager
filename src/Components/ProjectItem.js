61bf224bb4155fb1a29cba0d080794d61e7a75a4
Kevin Ke <kevinke@MacBook-Pro-1023.local>
2018-03-21 22:45:59 -0700

    first commit

---
+++ src/Components/ProjectItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class ProjectItem extends Component {
+  render() {
+    // console.log(this.props);
+    return (
+      <li className="Project Item">
+
+      </li>
+    );
+  }
+}
+
+export default ProjectItem;
